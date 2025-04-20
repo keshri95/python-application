382068e6f3044e33a893e3880af70671f32a392e
Ashish Keshri <keshri.ashish1@gmail.com>
2025-04-20 17:55:21 +0530

    done

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -2,12 +2,26 @@ import random
 
 choice_from_game = ['r', 'p', 's']
 
-user_choice = input("Rock, Paper, Scissors? (r/p/s): ").lower()
 
-guessed = random.choice(choice_from_game)
+# function to continue the game -------------
+def continue_game():
+    continue_choice = input("Do you want to play again? (y/n): ").lower()
+
+    if continue_choice == 'y':
+        print("Let's play again!")
+        return True
+    
+    elif continue_choice == 'n':
+        print("Thanks for playing!")
+        return False
+    
+    else:
+        print("Invalid input. Please enter 'y' or 'n'.")
+        return continue_game()
+        
 
 
-
+# Decide the winner -------------
 def winner_is(user, computer):
     if user == computer:
         print("It is a Tie!!!")
@@ -21,13 +35,29 @@ def winner_is(user, computer):
         print("You lose!!!")
 
 
+# main function check the game ----------------
 
-if user_choice in choice_from_game:
+def rock_paper_scissors():
+    while True:
 
-    print(f'You chose {user_choice}!!')
-    print(f'Computer chose {guessed}!!!')
-    winner_is(user_choice, guessed)
+        user_choice = input("Rock, Paper, Scissors? (r/p/s): ").lower()
+
+        guessed = random.choice(choice_from_game)
 
 
-else:
-    print("Invalid choice.")
+        if user_choice in choice_from_game:
+
+            print(f'You chose {user_choice}!!')
+            print(f'Computer chose {guessed}!!!')
+            winner_is(user_choice, guessed)
+
+            if not continue_game():
+            # continue_game()
+                break
+
+
+        else:
+            print("Invalid choice.")
+
+
+rock_paper_scissors()

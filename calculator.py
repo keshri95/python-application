2500c2f3474cf6f295eef422cad112fce7d4eb6b
Ashish Keshri <keshri.ashish1@gmail.com>
2025-04-22 09:32:24 +0530

    play_again_fn

--- calculator.py
+++ calculator.py
@@ -1,34 +1,74 @@
 
-first_input = int(input("Enter the first value!!!!"))
-second_input = int(input("Enter thr second value!!!!"))
+def play_again():
+    play_game = str(input("Enter your choice (y/n)").lower())
 
-# print(first_input)
-# print(second_input)
+    if play_game == 'y':
 
-user_choice = input("Enter your choice (Add/Sub/Mul/Div): ").lower()
-# print(user_choice)
+        print("Let's play again! ")
+        return True
+    
+    elif play_game == 'n':
 
-
-if user_choice == str("Add").lower():
-    operation =  first_input + second_input
-    print(operation)
-
-elif user_choice == str("Sub").lower():
-    operation = first_input - second_input
-    print(operation)
-
-elif user_choice == str("Mul").lower():
-    operation = first_input * second_input
-    print(operation)
-
-elif user_choice == str("Div").lower():
-
-    if second_input != 0:
-        operation = first_input // second_input
-        print(operation)
+        print("Thanks for playing!! ")
+        return False
+    
 
     else:
-        print("Out of bound!!! ")
+        print("Invalid input. Please enter 'y' or 'n'.")
 
-else:
-    print("Invalid choice!! ")
+        return play_again()
+
+
+# 
+def calculator():
+
+    while True:
+        try:
+            first_input = int(input("Enter the first value!!!!"))
+            second_input = int(input("Enter thr second value!!!!"))
+            user_choice = str(input("Enter your choice (Add/Sub/Mul/Div): ")).lower()
+        
+            if user_choice == str("Add").lower():
+                operation =  first_input + second_input
+                print(f"You have choose {user_choice}: ", operation)
+                play_again()
+
+
+            elif user_choice == str("Sub").lower():
+                operation = first_input - second_input
+                print(f"You have choose {user_choice}: ", operation)
+                # break
+                play_again()
+
+
+            elif user_choice == str("Mul").lower():
+                operation = first_input * second_input
+                print(f"You have choose {user_choice}: ", operation)
+                # break
+                play_again()
+
+            elif user_choice == str("Div").lower():
+
+                if second_input != 0:
+                    operation = first_input // second_input
+                    print(f"You have choose {user_choice}: ", operation)
+                    # break
+                    play_again()
+
+                else:
+                    print("Out of bound!!! ")
+                    # break
+                    play_again()
+
+
+            else:
+                print("Invalid choice!! ")
+                play_again()
+
+        except ValueError:
+                print("Please enter integer value! ")
+
+
+calculator()
+
+

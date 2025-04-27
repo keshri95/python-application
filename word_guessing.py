56d2b724a15c0b78f3d531231ee987cff1728a19
Ashish Keshri <keshri.ashish1@gmail.com>
2025-04-27 22:38:49 +0530

    word_guessing-game

---
+++ word_guessing.py
@@ -0,0 +1,21 @@
+ 
+
+
+user_name_is = input("What is your name? ").strip()
+
+words = ['rainbow', 'computer', 'science', 'programming',
+         'python', 'mathematics', 'player', 'condition',
+         'reverse', 'water', 'board', 'geeks']
+
+
+while True:
+
+    if user_name_is:
+
+        print("Good Luck!", user_name_is)
+        # print("Guess the characters: ")
+        break
+
+    else:
+        print("You haven't entered name! ")
+        break
